3b9773351927410c4f212ab6a5f722fbf539b8f9
Julio Ona <thinmanj@gmail.com>
2025-10-31 23:12:53 -0400

    chore: Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="code-analyzer",
-    version="0.2.1",
+    version="0.4.0",
     author="Julio",
     description="Deep source code analysis and documentation tool",
     long_description=long_description,
